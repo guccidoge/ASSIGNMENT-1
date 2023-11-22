fbc0239f72e397b00d06e2c8023d513f90489483
eilli <eilli@LAPTOP-D1VN7TNU>
2023-11-22 19:37:21 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'The following steps calculate the percentage profit from the sale of a stock
+        'Declare the variable item as string. Declare the variables regularPrice And discount as type Double.
+        'Assign “ketchup” to the variable item.
+        'Assign 1.80 to the variable regularPrice.
+        'Assign 0.27 to the variable discount.
+        'Display the phrase “RM1.53 is the sale of the ketchup”.
+        Dim item As String = "ketchup"
+        Dim regularPrice As Double = 1.8
+        Dim discount As Double = 0.27
+        Dim finalPrice As Double
+        finalPrice = regularPrice - discount
+        Label1.Text = finalPrice.ToString("C") & " is the sale of the " & item
+    End Sub
+End Class
